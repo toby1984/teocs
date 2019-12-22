8bbbcbfceb471f6569d0abd54bd7eecfcc065743
Tobias Gierke <tobias.gierke@code-sourcery.de>
2019-12-22 15:28:28 +0100

    wip

--- java/hackasm/src/main/java/de/codesourcery/hack/asm/IObjectCodeWriter.java
+++ java/hackasm/src/main/java/de/codesourcery/hack/asm/IObjectCodeWriter.java
@@ -2,6 +2,15 @@ package de.codesourcery.hack.asm;
 
 public interface IObjectCodeWriter extends AutoCloseable
 {
+    enum Segment {
+        RAM,
+        ROM;
+    }
+
+    void setCurrentSegment(Segment segment);
+
+    Segment currentSegment();
+
     int currentAddress();
 
     void writeWord(int value); // write 16 bits
--- java/hackasm/src/main/java/de/codesourcery/hack/vm/VMTranslator.java
+++ java/hackasm/src/main/java/de/codesourcery/hack/vm/VMTranslator.java
@@ -9,8 +9,16 @@ public class VMTranslator
 {
     private PrintWriter writer;
 
+    private int labelCount;
+
     public VMTranslator(PrintWriter writer) {
         this.writer = writer;
+    }
+
+    private String genLabel(String name) {
+        String result = "label_"+name+"_"+labelCount+":";
+        labelCount++;
+        return result;
     }
 
     /*
@@ -80,29 +88,195 @@ public class VMTranslator
          * RAM[5-12] - Holds the contents of the 'temp' segment
          * RAM[13-15] - Free to use by VM implementation for general purposes
          */
-        int ptrAdr;
+        writer.println("@"+ ptrFor(segment)); // A = "address register"
+        writer.println("D=M"); // D=Mem[A] -> D = ptr to start of segment
+        writer.println("@"+idx);
+        writer.println("A=D+A"); // A = (ptr to start of segment) + idx
+        writer.println("D=M"); // D=*((ptr to start of segment) + idx)
+        writer.println("@"+0); // A = 0 -> ToS ptr
+        writer.println("M=D"); // A = Mem[0] = Top of stack
+        writer.println("M=M-1"); // update ToS ptr
+    }
+
+    //   Pop the top stack value and store it in segment[index].
+    private void pop(MemorySegment segment, int idx) {
+
+        // calculate destination address
+        writer.println("@"+ptrFor(segment));
+        writer.println("D=M");
+        writer.println("@"+idx);
+        writer.println("D=D+A"); // A = destination address
+
+        writer.println("@13"); // store destination adr in tmp variable
+        writer.println("M=D");
+
+        writer.println("@"+0); // A = 0 -> ToS ptr
+        writer.println("M=M+1"); // bump ptr
+        writer.println("D=M");
+
+        writer.println("@13");
+        writer.println("M=D");
+    }
+
+    private void popIntoA() {
+        writer.println("@"+0); // A = 0 -> ToS ptr
+        writer.println("AM=M+1"); // bump ptr
+        writer.println("A=M");
+    }
+
+    private void popIntoD() {
+        writer.println("@"+0); // A = 0 -> ToS ptr
+        writer.println("AM=M+1"); // bump ptr
+        writer.println("D=M");
+    }
+
+    private void add() {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+        writer.println("D=D+A");
+        pushD();
+    }
+
+    private void sub() {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+        writer.println("D=A-D");
+        pushD();
+    }
+
+    private void neg() {
+        popIntoD(); // D = 2nd argument
+        writer.println("D=-D");
+        pushD();
+    }
+
+    private void writeJmp(String destination) {
+        writer.println("@"+destination);
+        writer.println("0;JMP");
+    }
+
+    private void eq()
+    {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+
+        writer.println("D=A-D");
+        final String eqLabel = genLabel("eq");
+        final String cont = genLabel("cont");
+        writer.println("@"+eqLabel);
+        writer.println("D;JEQ");
+        // -> not equals
+        writer.println("@0"); // true = -1 , false = 0
+        pushA();
+        writeJmp(cont);
+
+        writeLabel(eqLabel);
+        writer.println("@-1"); // true = -1 , false = 0
+        pushA();
+
+        writeLabel(cont);
+    }
+
+    private void gt()
+    {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+
+        // A > D
+        final String gtLabel = genLabel("gt");
+        final String cont = genLabel("cont");
+        writer.println("D=D-A");
+        writer.println("@"+gtLabel);
+        writer.println("D;JGT "+gtLabel);
+        // -> not greater
+        writer.println("@0"); //  false = 0
+        pushA();
+        writeJmp(cont);
+
+        writeLabel(gtLabel);
+        writer.println("@-1"); // true = -1
+        pushA();
+
+        writeLabel(cont);
+    }
+
+    private void lt()
+    {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+
+        // A < D
+        final String ltLabel = genLabel("gt");
+        final String cont = genLabel("cont");
+        writer.println("D=A-D");
+        writer.println("@"+ltLabel);
+        writer.println("D;JLT "+ltLabel);
+        // -> not less
+        writer.println("@0"); //  false = 0
+        pushA();
+        writeJmp(cont);
+
+        writeLabel(ltLabel);
+        writer.println("@-1"); // true = -1
+        pushA();
+
+        writeLabel(cont);
+    }
+
+    private void and()
+    {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+        writer.println("D=A&D");
+        pushD();
+    }
+
+    private void or()
+    {
+        popIntoD(); // D = 2nd argument
+        popIntoA(); // A = first argument
+        writer.println("D=A|D");
+        pushD();
+    }
+
+    private void not()
+    {
+        popIntoD(); // D = 2nd argument
+        writer.println("D=!D");
+        pushD();
+    }
+
+    private void writeLabel(String label) {
+        writer.println(label+":");
+    }
+
+
+    private void pushA() {
+
+        writer.println("D=A");
+        pushD();
+    }
+
+    private void pushD() {
+
+        writer.println("@"+0); // A = 0 -> ToS ptr
+        writer.println("A=M");
+        writer.println("M=D");
+
+        writer.println("@"+0); // A = 0 -> ToS ptr
+        writer.println("M=M-1"); // bump ptr
+    }
+
+    private int ptrFor(MemorySegment segment) {
         switch(segment)
         {
-            case ARGUMENT: ptrAdr = 2; break;
-            case LOCAL:    ptrAdr = 1; break;
-            case THIS:     ptrAdr = 3; break;
-            case THAT:     ptrAdr = 4 ; break;
+            case ARGUMENT: return 2;
+            case LOCAL:    return 1;
+            case THIS:     return 3;
+            case THAT:     return 4;
             default:
                 throw new IllegalStateException("Unexpected value: " + segment);
         }
-        writer.println("@"+ptrAdr); // A = "address register"
-        writer.println("D=A"); // D=Mem[A] =
-        writer.println("@"+idx);
-        writer.println("A=D+A"); // D=Mem[A] =
-        writer.println("D=M"); // D=Mem[A] =
-        writer.println("@"+0); // A = 0
-        writer.println("A=M"); // A = Mem[0] = Top of stack
-        writer.println("M=D"); // Top of stack =
-        writer.println("A=A-1");
-        writer.println("D=A");
-        writer.println("@"+0);
-        writer.println("M=D"); // update ToS ptr
-
     }
 
     public String translate(String source)
